37dd3dda847e1ee44834462d8b645398f17fd728
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-23 13:07:30 +0200

    refactor: format my code

--- Romans/Roman Numerals Encoder/index.js
+++ Romans/Roman Numerals Encoder/index.js
@@ -1,15 +1,28 @@
-function solution(number){
-    let finalRomanNumeral = '',
-         numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-         romans    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
- 
-   numbers.map((value, index) => {    
-     while (number >= value) {
-       finalRomanNumeral += romans[index];
-       number -= value;
-     }
-   });
-   
-   return finalRomanNumeral;
- }
- 
+function solution(number) {
+  let finalRomanNumeral = "",
+    numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    romans = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I",
+    ];
+
+  numbers.map((value, index) => {
+    while (number >= value) {
+      finalRomanNumeral += romans[index];
+      number -= value;
+    }
+  });
+
+  return finalRomanNumeral;
+}
