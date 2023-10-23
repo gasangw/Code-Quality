3b5ea06bb4c26a9ee20c45ca7915db29d410a75a
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-23 10:55:28 +0200

    refactor: improve code for exercise one

--- code Improvement/exercise1.js
+++ code Improvement/exercise1.js
@@ -1,6 +1,5 @@
-function calculateAverage(numbers) {
-    let sum = 0;
-    let count = 0;
+const calculateAverage = (numbers) => {
+    let sum = 0, count = 0;
     for (const number_ of numbers) {
       sum += number_;
       count++;
