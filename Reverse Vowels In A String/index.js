a06d98dc9a0f078487a0242a2bfbf45bf7d0bd32
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-23 10:42:16 +0200

    refactor: refactor code using eslint

--- Reverse Vowels In A String/index.js
+++ Reverse Vowels In A String/index.js
@@ -1,4 +1,4 @@
 function reverseVowels(string_) {
-    let vowels = [...string_.replaceAll(/[^aeiou]/gi, '')];
-  return string_.replaceAll(/[aeiou]/gi, _ => vowels.pop());
+    const vowels = [...string_.replaceAll(/[^aeiou]/gi, '')]
+    return string_.replaceAll(/[aeiou]/gi, ()=> vowels.pop())
 }
