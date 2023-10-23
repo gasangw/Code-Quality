0723a5a4f65c967c6c368359801da44786f987e3
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-23 12:52:08 +0200

    refactor: refactor exercise 3

--- code Improvement/exercise3.js
+++ code Improvement/exercise3.js
@@ -1,7 +1,3 @@
 function reverseString(string_) {
-    let reversed = "";
-    for (const index = string_.length - 1; index >= 0; index--) {
-      reversed += string_[index];
-    }
-    return reversed;
-  }
+  return [...string_].reverse().join('');
+}
